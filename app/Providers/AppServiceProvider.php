ab3fbd339dad30f09440fb94d750e8a0936d446e
Alex Renoki <rennokki@gmail.com>
2021-08-20 15:51:03 +0300

    Register thinker locally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('development')) {
+            $this->app->register(\Intonate\TinkerZero\TinkerZeroServiceProvider::class);
+        }
     }
 
     /**
