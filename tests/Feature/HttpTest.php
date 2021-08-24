8e8c93c615e4cf36a391fe4b7a6f37b3cca33a6f
Alex Renoki <rennokki@gmail.com>
2021-08-25 00:20:01 +0300

    Fixed assertions

--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -192,7 +192,7 @@ class HttpTest extends TestCase
             $this->assertNotNull($job->payload['response_time_ms']);
 
             return in_array($job->webhookUrl, ['https://webhook1.test', 'https://webhook2.test']) &&
-                in_array($job->payload['url'], ['https://webhook1.test', 'https://webhook2.test']) &&
+                $job->payload['url'] === 'https://google.test' &&
                 in_array($job->headers['Signature'], [
                     hash_hmac('sha256', json_encode($job->payload), 'secret1'),
                     hash_hmac('sha256', json_encode($job->payload), 'secret2'),
